20547b9f2a8fa8beecb6632b5f4ef4f9037d5a5d
Daniel <danisprodrigues22@gmail.com>
2026-01-20 16:01:08 -0300

    create functions

--- crud-usuarios.js
+++ crud-usuarios.js
@@ -5,6 +5,25 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+function perguntar(texto, callback) {
+    rl.question(texto, (resposta) => {
+        callback(resposta);
+    });
+}
+
+
+function acharIndicePorID(id) {
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+let usuarios = [];
+let proximoId = 1;
+
 function mostrarMenu(){
     console.log("");
     console.log("     CRUD USUÁRIOS");
@@ -17,6 +36,126 @@ function mostrarMenu(){
     console.log("0) SAIR");
     console.log("");
 }
+
+function cadastrarUsuario() {
+    console.log("\nCadastrar Usuário");
+
+    perguntar("Nome: ", (nome) => {
+
+                    nome = nome.trim();
+
+                    if (!nome) {
+                        console.log("ERRO: Preencha todos os campos.");
+                        return menu();
+                    }
+
+                    const usuario = {
+                        id: proximoId,
+                        nome: nome,
+                    };
+
+                    usuarios.push(usuario);
+                    proximoId++;
+
+                    console.log("Usuário cadastrado com sucesso! ID:", usuario.id);
+                    menu();
+
+
+    });
+}
+
+function listarUsuario() {
+    console.log("\nLISTA DE USUÁRIOS");
+
+    if (usuarios.length === 0) {
+        console.log("Nenhum usuário cadastrado.");
+        return menu();
+    }
+
+    for (let i = 0; i < usuarios.length; i++) {
+        const c = usuarios[i];
+        console.log(
+            `ID: ${c.id} | Nome: ${c.nome}`
+        );
+    }
+    menu();
+}
+
+function visualizarUsuario() {
+    perguntar("Digite o ID do usuário: ", (idStr) => {
+        const id = +idStr;
+
+        if (Number.isNaN(id)) {
+            console.log("Erro: ID inválido.");
+            return menu();
+        }
+
+        const pos = acharIndicePorID(id);
+        if (pos === -1) {
+            console.log("Usuário não encontrado.");
+            return menu();
+        }
+
+        const c = usuarios[pos];
+        console.log(
+            `ID: ${c.id} | Nome: ${c.nome}`
+        );
+        menu();
+    });
+}
+
+function editarUsuario() {
+    perguntar("Informe o ID do usuário: ", (idStr) => {
+        const id = +idStr;
+
+        if (Number.isNaN(id)) {
+            console.log("Erro: ID inválido.");
+            return menu();
+        }
+
+        const pos = acharIndicePorID(id);
+        if (pos === -1) {
+            console.log("Usuário não encontrado.");
+            return menu();
+        }
+
+        const usuario = usuarios[pos];
+
+        perguntar(`Nome [${usuario.nome}]: `, (novoNome) => {
+
+
+                        novoNome = novoNome.trim();
+
+                        if (novoNome) usuario.nome = novoNome;
+
+                        console.log("Usuário editado com sucesso!");
+                        menu();
+                    });
+
+    });
+}
+
+function deletarUsuario() {
+    perguntar("Digite o ID do usuário: ", (idStr) => {
+        const id = +idStr;
+
+        if (Number.isNaN(id)) {
+            console.log("Erro: ID inválido.");
+            return menu();
+        }
+
+        const pos = acharIndicePorID(id);
+        if (pos === -1) {
+            console.log("Usuário não encontrado.");
+            return menu();
+        }
+
+        usuarios.splice(pos, 1);
+        console.log("Usuário deletado com sucesso.");
+        menu();
+    });
+}
+
 
 function menu(){
     mostrarMenu();
@@ -40,3 +179,4 @@ function menu(){
     })
 }
 
+menu();
